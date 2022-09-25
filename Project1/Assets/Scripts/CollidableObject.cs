b9f9292d252c65c8c5a57b22088fe2733c76952d
Proviso12 <15proviso@gmail.com>
2022-09-25 10:57:18 -0400

    pushing a new project 1

---
+++ Project1/Assets/Scripts/CollidableObject.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollidableObject : MonoBehaviour
+{
+    public bool isCurrentlyColliding = false;
+    
+    public float radius = 1f;
+    [HideInInspector]
+    public SpriteRenderer spriteRenderer;
+
+    public SpriteRenderer SpriteRenderer{ get { return spriteRenderer; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if colliding, turn me red
+        if(isCurrentlyColliding)
+        {
+            spriteRenderer.color = Color.red;
+        }
+        else
+        {
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+    public void ResetCollision()
+    {
+        isCurrentlyColliding = false;
+    }
+    public void RegisterCollision()
+    {
+        
+    }
+    private void OnDrawGizmos()
+    {
+        if(isCurrentlyColliding)
+        { 
+            Gizmos.color = Color.red;
+        }
+        else
+        {
+            Gizmos.color = Color.green;
+        }
+        
+        Gizmos.DrawWireCube(transform.position, spriteRenderer.bounds.size);
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
